7ada4e152a280860915c33724aa2a8a1ef1abb9a
Joseph Nowell <joseph.m.nowell@gmail.com>
2026-02-14 10:04:09 -0500

    Codex: Renamed to VibeWrite2k

--- VibePlatform/Models/Document.cs
+++
@@ -1,7 +0,0 @@
-namespace VibePlatform.Models;
-
-public class Document
-{
-    public string? FilePath { get; set; }
-    public string FileName => FilePath != null ? System.IO.Path.GetFileName(FilePath) : "Untitled";
-}
--- VibePlatform/Models/VersionHistory.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections.Generic;
-
-namespace VibePlatform.Models;
-
-public class VersionHistory
-{
-    public int Version { get; set; } = 1;
-    public List<VersionCommit> Commits { get; set; } = new();
-}
